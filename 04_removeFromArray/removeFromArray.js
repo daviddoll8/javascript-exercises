8220ed0e3a030fdbee42946e707f0d7262f32911
David Doll <doll.david0395@gmail.com>
2023-09-02 23:02:48 -0500

    complete exercise 4, creating a function to remove elements from a given array if they are also given as arguments and return the new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const retArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            retArray.push(item);
+        }
+    });
+    return retArray;
 };
 
 // Do not edit below this line
